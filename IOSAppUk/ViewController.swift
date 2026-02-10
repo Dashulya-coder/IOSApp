bbe7291d4ce79f3039dedb3ffddaddec4e179457
boss <a@no.pics>
2026-02-10 22:22:44 +0200

    Initial Commit

---
+++ IOSAppUk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSAppUk
+//
+//  Created by Daria Ukshe on 10.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
