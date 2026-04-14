a1252a3064f473957b6128bc0134281284a6462d
boss <a@no.pics>
2026-04-14 13:25:18 +0300

    feat: add notification support for post updates

---
+++ IOSAppUk/Support/NotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName.swift
+//  IOSAppUk
+//
+//  Created by Daria Ukshe on 14.04.2026.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let localPostsDidChange = Notification.Name("localPostsDidChange")
+}
