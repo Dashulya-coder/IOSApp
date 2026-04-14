004268809fd846f2fb05c491398592885c1b7e54
boss <a@no.pics>
2026-04-14 13:24:54 +0300

    fix: remove storyboard launch configuration from SwiftUI app

---
+++ IOSAppUk/App/IOSAppUk.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAppUk.swift
+//  IOSAppUk
+//
+//  Created by Daria Ukshe on 14.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct IOSAppUk: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
